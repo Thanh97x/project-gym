75d6c9604305fbcbbf502f652269b81af3bebed3
Thanh97x <122072000+Thanh97x@users.noreply.github.com>
2023-07-07 18:13:10 +0700

    delete source

--- aspnet-core/src/Demotht.Application/Authorization/Accounts/Dto/RegisterOutput.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Demotht.Authorization.Accounts.Dto
-{
-    public class RegisterOutput
-    {
-        public bool CanLogin { get; set; }
-    }
-}
--- aspnet-core/src/Demotht.Core/Configuration/AppSettingNames.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Demotht.Configuration
-{
-    public static class AppSettingNames
-    {
-        public const string UiTheme = "App.UiTheme";
-    }
-}
